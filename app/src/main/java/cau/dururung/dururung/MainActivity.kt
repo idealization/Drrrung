eb611f10446c7339c43eae14ee4b54857eae31d8
Myeongwon Choi <auspic7@gmail.com>
2021-07-06 10:15:09 +0900

    Add empty Android app project

---
+++ app/src/main/java/cau/dururung/dururung/MainActivity.kt
@@ -0,0 +1,11 @@
+package cau.dururung.dururung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
