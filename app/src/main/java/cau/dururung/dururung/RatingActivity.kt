4ebe9a0602271e0da6b0f292c856b582c41b8645
ant67410 <ant67410@gmail.com>
2021-07-09 19:13:25 +0900

    use rating bar and add save button

--- app/src/main/java/cau/dururung/dururung/RatingActivity.kt
+++ app/src/main/java/cau/dururung/dururung/RatingActivity.kt
@@ -2,10 +2,23 @@ package cau.dururung.dururung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cau.dururung.dururung.databinding.ActivityMainBinding
+import cau.dururung.dururung.databinding.ActivityRatingBinding
+
 
 class RatingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRatingBinding
+    var ratingValue: Float = 0.0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rating)
+        binding = ActivityRatingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            ratingValue = rating
+        }
+        binding.save.setOnClickListener {
+            Toast.makeText(this, "${ratingValue}점", Toast.LENGTH_SHORT).show()
+        }
     }
 }
