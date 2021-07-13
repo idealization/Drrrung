838b745cb36b12ef2691d8ee97c70ee16592e5ae
ant67410 <ant67410@gmail.com>
2021-07-13 21:06:26 +0900

    Make Alarm View

---
+++ app/src/main/java/cau/dururung/dururung/AlarmActivity.kt
@@ -0,0 +1,16 @@
+package cau.dururung.dururung
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+
+
+}
