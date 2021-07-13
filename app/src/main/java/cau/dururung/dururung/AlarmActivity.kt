2089b04dd802c952fac565d29c09d921b19b3eee
ant67410 <ant67410@gmail.com>
2021-07-13 23:22:32 +0900

    Update Alarm View with Time

--- app/src/main/java/cau/dururung/dururung/AlarmActivity.kt
+++ app/src/main/java/cau/dururung/dururung/AlarmActivity.kt
@@ -3,13 +3,20 @@ package cau.dururung.dururung
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AlarmActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+
+
+
     }
 
 
