7c8604c4e06453b2d86e4eb20c32efe37be54ee1
ant67410 <ant67410@gmail.com>
2021-07-16 23:29:34 +0900

    Add snooze and Intent

--- app/src/main/java/cau/dururung/dururung/AlarmActivity.kt
+++ app/src/main/java/cau/dururung/dururung/AlarmActivity.kt
@@ -1,18 +1,16 @@
 package cau.dururung.dururung
 
-
-import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
 import cau.dururung.dururung.databinding.ActivityAlarmBinding
-import cau.dururung.dururung.databinding.ActivityRatingBinding
-import java.lang.Exception
+import java.util.*
+
 
 class AlarmActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAlarmBinding
@@ -32,17 +30,45 @@ class AlarmActivity : AppCompatActivity() {
 
         //val alarmName = "ringstone"
         //val alarm: Uri = Uri.parse("android.resource://"+packageName+"/raw/ringstone.mp3")
-        val alarm: Uri = Uri.parse("android.resource://"+packageName+"/"+R.raw.ringstone)
-        //val alarm: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        //val alarm: Uri = Uri.parse("android.resource://"+packageName+"/"+R.raw.ringstone)
+        val alarm: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
         r2 = RingtoneManager.getRingtone(this, alarm)
+        /*
+        val mMediaPlayer = MediaPlayer()
+        try {
+            mMediaPlayer.setDataSource(this, alarm)
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM)
+            mMediaPlayer.isLooping = true
+            mMediaPlayer.prepare()
+            mMediaPlayer.start()
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+        */
         r2.play()
     } catch (e:Exception){
         e.printStackTrace()
     }
+        var intent = Intent(this@AlarmActivity, RatingActivity::class.java)
         binding.stopBtn.setOnClickListener {
             r2.stop()
             vib.cancel()
+            intent.putExtra("isSnooze",false)
+            setResult(RESULT_OK, intent)
+            finish()
         }
+        binding.snooze.setOnClickListener {
+            r2.stop()
+            vib.cancel()
+            intent.putExtra("isSnooze",true)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
 
     }
 
